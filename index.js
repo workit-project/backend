deab34dea0cc7e8d4517c0f71d3248ae40109c62
Chance Hudson <jchancehud@gmail.com>
2019-09-09 19:42:39 -0500

    add mongoose connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
 const app = express()
 
 app.use(express.json())
@@ -7,6 +9,14 @@ app.use((_, res, next) => {
   res.set('Access-Control-Allow-Origin', '*')
   res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
   res.set('Access-Control-Allow-Headers', 'content-type')
+  next()
+})
+
+app.use(async (_, __, next) => {
+  await mongoose.connect(process.env.DB_URI, {
+    connectTimeoutMs: 5000,
+    useNewUrlParser: true,
+  })
   next()
 })
 
